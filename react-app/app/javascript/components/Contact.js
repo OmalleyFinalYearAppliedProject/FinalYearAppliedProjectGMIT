7098f099e1fb6113fc639bbb249f32a0ab24e9a5
root <you@example.com>
2020-10-29 14:35:42 +0000

    Began Page Routing

---
+++ react-app/app/javascript/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Contact extends React.Component {
+
+	render(){
+		return(
+
+<div>This is the Contact page </div>
+			)
+
+	}
+
+}
+
+export default Contact
---
+++ react-app/app/javascript/packs/index.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRoute as Router, Route} from 'react-router-dom'
+import PropTypes from 'prop-types'
+import App from '../components/App'
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+  <Router>
+  <Route path= "/" component={App}/>
+    <Router/>,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
