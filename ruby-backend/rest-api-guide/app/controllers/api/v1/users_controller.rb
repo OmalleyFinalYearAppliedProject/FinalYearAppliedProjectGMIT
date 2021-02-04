bfcf885fc64f832aa5698f6d3af1767f6ad8a25f
Tomas O'Malley <>
2021-02-04 18:25:30 +0000

    Added Ruby Backend

---
+++ ruby-backend/rest-api-guide/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,58 @@
+class Api::V1::UsersController < ApplicationController
+
+
+    # GET /users
+    def index 
+        @users = User.all 
+        render json: @users
+    end
+
+
+     # GET /user/:id
+     def show 
+        @user = User.find(params[id]) 
+        render json: @user
+    end
+
+
+     # POST /users
+     def create 
+        @users = User.new(user_params) 
+        if @user.save
+            render json: @user
+        else 
+            render error: { error: 'unable to create user.'}, status:400
+        end
+    end
+
+
+    # put /users/:id
+    def update 
+        @user = User.find(params[:id])
+        if @user
+            @user.update(user_params)
+            render json: { message: 'user successfully updated'}, status: 200
+        else 
+            render json: { error: 'unable to update user'}, status: 400
+        end
+    end
+
+
+    # Delete /users/:id
+    def destroy
+        @user = User.find(params[:id])
+        if @user
+            @user.destroy
+            render json: { message: 'User successfully deleted. '} , status: 200
+        else 
+            render json: { error: 'Unable to delete user. '} , status: 400
+        end
+    end
+
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
