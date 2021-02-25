7c429ab12cd1c938be470e1d9510ef5a25f6f1a6
Tomas O'Malley <>
2021-02-25 15:00:46 +0000

    Fixed styling issues in user activity pages

--- ELEARNINGGMITAPP/app/src/main/java/com/example/e_learning_gmit_app/CalenderActivity.kt
+++ ELEARNINGGMITAPP/app/src/main/java/com/example/e_learning_gmit_app/CalenderActivity.kt
@@ -2,10 +2,17 @@ package com.example.e_learning_gmit_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CalenderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calender)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+
     }
+
+
 }
