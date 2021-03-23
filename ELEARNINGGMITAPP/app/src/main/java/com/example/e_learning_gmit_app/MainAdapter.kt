4e8217c3af2460740be2392b9d21e782d1363b89
Tomas Omalley <tomasomalley@tomass-mbp.home>
2021-03-23 13:43:02 +0000

    Added JSON backend parsing

--- ELEARNINGGMITAPP/app/src/main/java/com/example/e_learning_gmit_app/MainAdapter.kt
+++ ELEARNINGGMITAPP/app/src/main/java/com/example/e_learning_gmit_app/MainAdapter.kt
@@ -4,33 +4,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.quiz_row.view.*
 
-class MainAdapter: RecyclerView.Adapter<CustomViewHolder>(){
+class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>(){
 
+
+
+    val titles = listOf("who", "what","where")
 
 
     // Number of Items
     override fun getItemCount(): Int {
 
-        return  3
+        return  homeFeed.users.count()
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
 
         // Create a view
-        val layoutInflater = LayoutInflater.from(parent.context)
+        val layoutInflater = LayoutInflater.from(parent?.context)
         val cellForRow = layoutInflater.inflate(R.layout.quiz_row , parent ,false)
         return CustomViewHolder(cellForRow)
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+
+        val user = homeFeed.users.get(position)
+    holder?.view?.quizTextView?.text = user.username
+
+
     }
-
-
 
 }
 
-class CustomViewHolder(v: View): RecyclerView.ViewHolder(v) {
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
 
 
 }
--- ELEARNINGGMITAPP/app/src/main/java/com/example/e_learning_gmit_app/QuizFeed.kt
+++ ELEARNINGGMITAPP/app/src/main/java/com/example/e_learning_gmit_app/QuizFeed.kt
@@ -6,7 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_quiz_feed.*
+import okhttp3.*
+import java.io.IOException
 
 class QuizFeed : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,50 @@ class QuizFeed : AppCompatActivity() {
         //recyclerView_main.setBackgroundColor(Color.BLUE)
 
         recyclerView_main.layoutManager = LinearLayoutManager(this)
-        recyclerView_main.adapter = MainAdapter()
+      //  recyclerView_main.adapter = MainAdapter()
+
+
+        fetchJson()
+    }
+
+    fun fetchJson(){
+        println("Attempting to Fetch JSON")
+
+
+        var url = "https://piece-of-cake-learning.herokuapp.com/api/v1/users"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback {
+
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+                val homeFeed =  gson.fromJson(body, HomeFeed::class.java )
+
+
+                runOnUiThread {
+                    recyclerView_main.adapter = MainAdapter(homeFeed)
+                }
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Failed to exec")
+            }
+
+
+        })
     }
 }
+
+class HomeFeed(val users: List<User>)
+
+class User(val id: Int ,val username: String  , val password: String , val created_at: String , val updated_at: String )
+
+//class Channel(val name:String , val profileImageUrl: String)
+
+//[{"id":1,"username":"Oliver","password":"password","created_at":"2021-02-16T22:52:47.440Z","updated_at":"2021-02-16T22:52:47.440Z"}]
