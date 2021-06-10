6eafc00cef6b42e12087d4910cf34c7d3b660160
tomas <g00361128@gmit.ie>
2021-06-10 21:47:49 +0100

    Removed Android Wear bug

--- ELEARNINGGMITAPP/app/src/main/java/com/example/e_learning_gmit_app/BoardsActivity.kt
+++ ELEARNINGGMITAPP/app/src/main/java/com/example/e_learning_gmit_app/BoardsActivity.kt
@@ -1,15 +1,14 @@
 package com.example.e_learning_gmit_app
 
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
+import androidx.appcompat.app.AppCompatActivity
 
-class BoardsActivity : WearableActivity() {
+class BoardsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_boards)
 
         // Enables Always-on
-        setAmbientEnabled()
     }
 }
