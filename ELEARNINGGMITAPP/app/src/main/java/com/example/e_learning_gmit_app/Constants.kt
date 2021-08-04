08638aec7b1f20aa4232b7c68f7bd574487fa10d
Tomas O'Malley <>
2021-08-04 19:51:00 +0100

    Added Unix style Questions

--- ELEARNINGGMITAPP/app/src/main/java/com/example/e_learning_gmit_app/Constants.kt
+++ ELEARNINGGMITAPP/app/src/main/java/com/example/e_learning_gmit_app/Constants.kt
@@ -12,54 +12,54 @@ object Constants{
             , "list" , "dir" , "lir" , 1
         )
 
-        val que2 = Question(2, "Unix Command TEST 2 " , R.drawable.linux, "ls"
-            , "list" , "dir" , "lir" , 1
+        val que2 = Question(2, "Which command is used to sort the lines of data in a file in reverse order" , R.drawable.linux, "sort"
+            , "sh" , "st" , "none of the above" , 4
 
         )
 
-        val que3 = Question(3, "Unix Command TEST 3" , R.drawable.linux, "ls"
-            , "list" , "dir" , "lir" , 1
-
-
-        )
-
-        val que4 = Question(4, "Unix Command TEST 4" , R.drawable.linux, "ls"
-            , "list" , "dir" , "lir" , 1
+        val que3 = Question(3, "Which command is used to display the top of the file?" , R.drawable.linux, "cat"
+            , "head" , "more" , "grep" , 2
 
 
         )
 
-        val que5 = Question(5, "Unix Command TEST 5" , R.drawable.linux, "ls"
-            , "list" , "dir" , "lir" , 1
+        val que4 = Question(4, "Which command is used to remove a directory?" , R.drawable.linux, "rd"
+            , "rmdir" , "dldir" , "rdir" , 2
 
 
         )
 
-        val que6 = Question(6, "Unix Command TEST 6" , R.drawable.linux, "ls"
-            , "list" , "dir" , "lir" , 1
+        val que5 = Question(5, "Which of the following keys is used to replace a single character with new text?" , R.drawable.linux, "S"
+            , "s" , "r" , "C" , 2
 
 
         )
 
-        val que7 = Question(7, "Unix Command TEST 7" , R.drawable.linux, "ls"
-            , "list" , "dir" , "lir" , 1
+        val que6 = Question(6, "Which command is used to extract specific columns from the file?" , R.drawable.linux, "cat"
+            , "cut" , "grep" , "paste" , 2
 
 
         )
 
-        val que8 = Question(8, "Unix Command TEST 8" , R.drawable.linux, "ls"
-            , "list" , "dir" , "lir" , 1
-
-
-        )
-        val que9 = Question(9, "Unix Command TEST 9" , R.drawable.linux, "ls"
-            , "list" , "dir" , "lir" , 1
+        val que7 = Question(7, "Which of the following commands is used to display the directory attributes rather than its contents?" , R.drawable.linux, "ls -l -d"
+            , "ls -l" , "ls -x" , "ls -F" , 1
 
 
         )
 
-        val que10 = Question(10, "Unix Command TEST 9" , R.drawable.linux, "ls"
-            , "list" , "dir" , "lir" , 1
+        val que8 = Question(8, "Which of the following files will displayed by this command cat *ch*" , R.drawable.linux, "ls"
+            , "list" , "dir" , "lir" , 4
+
+
+        )
+        val que9 = Question(9, "Which of the following keys is used to move the cursor to the end of the paragraph" , R.drawable.linux, "}"
+            , "{" , "|" , "$" , 1
+
+
+        )
+
+        val que10 = Question(10, "Which command is used to remove a file?" , R.drawable.linux, "remove"
+            , "rm" , "mv" , "del" , 2
 
 
         )
